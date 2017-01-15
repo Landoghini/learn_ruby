a6b7faf9321fd173fefcfd1b3d24f2baa52b3582
Alando Appling <alandoappling@gmail.com>
2017-01-15 12:33:08 -0800

    Finished calculator

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,12 @@
 #write your code here
+def ftoc(num)
+	(num-32)*5/9
+end
+
+def ctof(num)
+	num.to_f * 9/5 + 32
+end
+
+ftoc(32)
+ftoc(98.6)
+ctof(0)
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,20 @@
 #write your code here
+def add(a,b)
+	return a+b
+end
+
+add(0,0)
+
+def subtract(a,b)
+	return a-b
+end
+
+def sum(numbers)
+	numbers.inject(0) { |total, number| total+number }
+end
+
+def multiply (numbers)
+	 result = 1
+	 numbers.each {|it| result*=it}
+	 result
+end
