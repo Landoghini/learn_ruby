6c708e983af13152940afd8436486418b509df89
Alando Appling <alandoappling@gmail.com>
2017-01-28 20:22:32 -0800

    Finished timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,18 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize
+  	@seconds = 00
+  end
+
+  def time_string
+  	sec = @seconds % 60
+  	min = (@seconds - sec)/60 % 60
+  	hr = (@seconds - min*60)/3600
+  	padded(hr)+":"+padded(min)+":"+padded(sec)
+  end
+
+  def padded(num)
+  	num.to_s.length ==1? "0#{num}" : "#{num}"
+  end
 end
