31c13c1675d40567307a7137cc1275bb0e51dee5
Alando Appling <alandoappling@gmail.com>
2017-01-28 19:49:30 -0800

    Book titles finished

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,17 @@
-class Book
-# write your code here
+
+
+
+class Book # write your code here    
+  attr_accessor :title 
+
+  def title 
+    not_caps = ['a','an','the','in','and','on','of'] 
+    words = @title.split(' ') 
+    words.each do |word| 
+      if !not_caps.include?(word) or words.index(word) == 0 
+        word.capitalize! 
+      end 
+    end 
+    words.join(' ') 
+  end 
 end
